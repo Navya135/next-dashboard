3238dd813a9df2e7f82e3cdbe9ebb0985a2e2f19
Navya135 <84073427+Navya135@users.noreply.github.com>
2023-07-03 14:44:50 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,10 @@
 import React from "react";
 import { useState,useEffect } from "react";
 import Image from 'next/image';
+import Stories from "../components/constants";
+
 
 export default function Home(showNav, setShowNav) {
-  
-   const Stories = [
-    {
-      id:1,
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-      path:"/image4.jpg"
-    },
-    {
-        id:2,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        path:"/image2.jpg"
-    },
-    {
-        id:3,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        path:"/image3.jpg"
-    },
-    {
-        id:4,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        path:"/image4.jpg"
-    },
-    {
-        id:5,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        path:"/image4.jpg"
-    },
-    {
-        id:6,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        path:"/image2.jpg"
-    },
-    {
-        id:7,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        path:"/image3.jpg"
-    },
-    {
-        id:8,
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        path:"/image4.jpg"
-    },
-  ];
-
   
   return (
     <>
